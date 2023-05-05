56912d11d70c5aaf65ebc6e0cbcbe510f1a4f5c5
Guillermina Lorenzo Martinez <glorenzo2001@gmail.com>
2023-05-05 12:00:45 +0100

    Created event-list module css, updated event-list component with style

--- components/events/event-list.js
+++ components/events/event-list.js
@@ -1,10 +1,12 @@
 import EventItem from "./event-item";
 
+import EventListStyle from "./event-list.module.css";
+
 export default function EventList(props) {
   const { items } = props;
 
   return (
-    <ul>
+    <ul className={EventListStyle.list}>
       {items.map((event) => (
         <EventItem
           key={event.id}
