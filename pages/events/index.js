928ad460b502f6499d51b2731f7fe2f5558794e4
Guillermina Lorenzo Martinez <glorenzo2001@gmail.com>
2023-05-05 16:06:33 +0100

    Updated AllEvents page to show EventList component

--- pages/events/index.js
+++ pages/events/index.js
@@ -1,7 +1,12 @@
+import { getAllEvents } from "../../dummy-data";
+import EventList from "../../components/events/event-list";
+
 export default function AllEventsPage() {
+  const event = getAllEvents();
+
   return (
     <div>
-      <h1>All Events Page</h1>
+      <EventList items={event} />
     </div>
   );
 }
