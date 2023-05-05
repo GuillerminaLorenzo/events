7f22fa0b8716c37c4848ff3bbcb3a628ec166273
Guillermina Lorenzo Martinez <glorenzo2001@gmail.com>
2023-05-05 11:23:30 +0100

    Updated event-item component to handle data and show it

--- components/events/event-item.js
+++ components/events/event-item.js
@@ -3,20 +3,30 @@ import Link from "next/link";
 export default function EventItem(props) {
   const { id, title, date, location, image } = props;
 
+  const formattedDate = new Date(date).toLocaleDateString("es-US", {
+    day: "numeric",
+    month: "long",
+    year: "numeric",
+  });
+
+  const formattedAddress = location.replace(", ", "\n");
+
+  const formattedLink = `/events/${id}`;
+
   return (
     <li>
-      <img src="" alt="" />
+      <img src={"/" + image} alt={title} />
       <div>
-        <h2>Title</h2>
+        <h2>{title}</h2>
         <div>
-          <time>Date</time>
+          <time>{formattedDate}</time>
         </div>
         <div>
-          <address>Location</address>
+          <address>{formattedAddress}</address>
         </div>
       </div>
       <div>
-        <Link href="/">Explore event</Link>
+        <Link href={formattedLink}>Explore event</Link>
       </div>
     </li>
   );
