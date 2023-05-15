96a8b1c376080d4f3f5547e3715e4dff644e18c5
Guillermina Lorenzo Martinez <glorenzo2001@gmail.com>
2023-05-15 16:30:57 +0100

    Updated EventDetail page, added Head with title and meta

--- pages/events/[eventId].js
+++ pages/events/[eventId].js
@@ -1,10 +1,10 @@
 import { Fragment } from "react";
+import Head from "next/head";
 
 import { getEventById, getFeaturedEvents } from "../../helpers/api-utils";
 import EventSummary from "../../components/event-detail/event-summary";
 import EventLogistics from "../../components/event-detail/event-logistics";
 import EventContent from "../../components/event-detail/event-content";
-import ErrorAlert from "../../components/ui/error-alert";
 
 export default function EventDetailPage(props) {
   const event = props.selectedEvent;
@@ -19,6 +19,10 @@ export default function EventDetailPage(props) {
 
   return (
     <Fragment>
+      <Head>
+        <title>{event.title}</title>
+        <meta name="description" content={event.description} />
+      </Head>
       <EventSummary title={event.title} />
       <EventLogistics
         date={event.date}
