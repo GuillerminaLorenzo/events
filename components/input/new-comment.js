6c94d924cd9c12f3581a221a5f2eb8a5abe5f03e
Guillermina Lorenzo Martinez <glorenzo2001@gmail.com>
2023-05-17 16:11:40 +0100

    Updated sendCommentHandler component and module, form button style and form onSubmit

--- components/input/new-comment.js
+++ components/input/new-comment.js
@@ -36,7 +36,7 @@ export default function NewComment(props) {
   }
 
   return (
-    <form className={NewCommentStyle.form}>
+    <form className={NewCommentStyle.form} onSubmit={sendCommentHandler}>
       <div className={NewCommentStyle.row}>
         <div className={NewCommentStyle.control}>
           <label htmlFor="email">Your email</label>
@@ -52,7 +52,7 @@ export default function NewComment(props) {
         <textarea id="comment" rows="5" ref={commentInputRef}></textarea>
       </div>
       {isInvalid && <p>Please enter a valid email address and comment!</p>}
-      <button>Submit</button>
+      <button className={NewCommentStyle.btn}>Submit</button>
     </form>
   );
 }
