3bf5d954785f1853aa2bab4101bbb3c8e7fcbf6d
Guillermina Lorenzo Martinez <glorenzo2001@gmail.com>
2023-05-19 17:29:13 +0100

    Updated notification context to show and hide notifications

--- store/notification-context.js
+++ store/notification-context.js
@@ -7,6 +7,22 @@ const NotificationContext = createContext({
 });
 
 export function NotificationContextProvider(props) {
+  const [activeNotification, setActiveNotification] = useState();
+
+  function showNotificationHandler(notificationData) {
+    setActiveNotification(notificationData);
+  }
+
+  function hideNotificationHandler() {
+    setActiveNotification(null);
+  }
+
+  const context = {
+    notification: activeNotification,
+    showNotification: showNotificationHandler,
+    hideNotification: hideNotificationHandler,
+  };
+
   return (
     <NotificationContext.Provider value={context}>
       {props.children}
