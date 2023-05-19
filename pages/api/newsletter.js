a3d657c31e1e85b42049707e64d4432a9264c552
Guillermina Lorenzo Martinez <glorenzo2001@gmail.com>
2023-05-19 14:56:35 +0100

    Updated api/newsletter with try/catch and refactored database connection

--- pages/api/newsletter.js
+++ pages/api/newsletter.js
@@ -1,4 +1,4 @@
-import { MongoClient } from "mongodb";
+import { connectDatabase, insertDocument } from "../../helpers/db-utils";
 
 export default async function handler(req, res) {
   if (req.method === "POST") {
@@ -9,15 +9,22 @@ export default async function handler(req, res) {
       return;
     }
 
-    const client = await MongoClient.connect(
-      "mongodb+srv://guille:Amelia210217@cluster0.pqvtrsw.mongodb.net/?retryWrites=true&w=majority"
-    );
+    let client;
 
-    const db = client.db("events");
+    try {
+      client = await connectDatabase();
+    } catch (error) {
+      res.status(500).json({ message: "Connecting to the database failed!" });
+      return;
+    }
 
-    await db.collection("emails").insertOne({ email: userEmail });
-
-    client.close();
+    try {
+      await insertDocument(client, "emails", { email: userEmail });
+      client.close();
+    } catch (error) {
+      res.status(500).json({ message: "Inserting data failed!" });
+      return;
+    }
 
     res.status(201).json({ message: "Signed up!" });
   }
