5de2d24bcc59c8914b09e25fae51a5aee0a41d8f
Guillermina Lorenzo Martinez <glorenzo2001@gmail.com>
2023-05-19 11:10:12 +0100

    Updated CommentList component to use _id

--- components/input/comment-list.js
+++ components/input/comment-list.js
@@ -6,7 +6,7 @@ export default function CommentList(props) {
   return (
     <ul className={CommentListStyle.comments}>
       {items.map((item) => (
-        <li key={item.id}>
+        <li key={item._id}>
           <p>{item.text}</p>
           <div>
             By <address>{item.name}</address>
